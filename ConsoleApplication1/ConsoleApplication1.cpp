8dc6314a5fa4e12ec1e2bc2d455989b647f9390b
Lime_Coconut <cosmina.iosifescu@gmail.com>
2020-11-04 11:26:43 +0100

    Loading CupcakeGame to new repo
    Simple SFML game - building out. Remaining to build: HUD and TimeBar
    
    Co-Authored-By: Bruno Mikus <3471865+bikush@users.noreply.github.com>

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+/*A simple game where the player has to click on
+sprites of cupcakes that randomly fall on the screen to gain points.
+*/
+#include<SFML/Graphics.hpp>
+#include<iostream>
+#include"Engine.h"
+
+
+TextureHolder th;
+
+
+
+int main()
+{
+    Engine engine;
+
+    engine.run();
+
+
+
+    return 0;
+}
