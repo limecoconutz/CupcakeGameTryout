2c72c4687076f5e2dc37a21fa6c488e0dbcf48b7
Lime_Coconut <cosmina.iosifescu@gmail.com>
2020-11-18 11:50:48 +0100

    Moved the draw() function from its separate .cpp file into Engine.cpp

--- ConsoleApplication1/Engine.cpp
+++ ConsoleApplication1/Engine.cpp
@@ -46,3 +46,20 @@ void Engine::run()
 
 
 }
+
+
+void Engine::draw()
+{
+	//Clear the last frame
+	m_Window.clear(Color::Cyan);
+
+	//Draw the background
+	m_Window.draw(m_BackgroundSprite);
+
+	//Spawn and draw the cupcakes
+	cupcake.update(m_Window);
+	cupcake.draw(m_Window);
+
+	m_Window.display();
+
+}
