4e6d08ec7272b0e4e84f3c0d7e090af195bcd8a0
Lime_Coconut <cosmina.iosifescu@gmail.com>
2020-11-18 11:59:25 +0100

    Moved the Input and Mouse functions from their separate .cpp files to Engine.cpp

--- ConsoleApplication1/Engine.cpp
+++ ConsoleApplication1/Engine.cpp
@@ -2,13 +2,9 @@
 using namespace sf;
 TextureHolder th;
 
-//Public functions only here (private functions in Input.cpp, Update.cpp and Draw.cpp respectively)
-
 
 Engine::Engine()
 {
-
-
 	//Get the screen resolution and create an SFML window and fullscreen view
 	Vector2f resolution;
 	resolution.x = VideoMode::getDesktopMode().width;
@@ -56,5 +52,65 @@ void Engine::draw()
 	cupcake.draw(m_Window);
 
 	m_Window.display();
+
+}
+
+
+void Engine::input()
+{
+	Event event;
+	updateMousePosition();
+
+	//Handle the player quitting
+	if (Keyboard::isKeyPressed(Keyboard::Escape))
+	{
+		m_Window.close();
+
+	}
+
+	////Handle the player starting the game
+	//if (Keyboard::isKeyPressed(Keyboard::Return))
+	//{m_Playing = true;} else {m_Playing = false;}
+
+	//Handle Mouse clicking
+	if (Mouse::isButtonPressed(Mouse::Left) && m_Playing == true)
+	{
+
+		std::vector<sf::Sprite>& cupcakes = cupcake.m_spriteVector;
+
+		for (int c = 0; c < cupcakes.size(); c++)
+		{
+			//if the cupcake has left the screen
+			if (cupcakes[c].getPosition().y >= m_Window.getSize().y)
+			{
+				//delete the cupcake
+				cupcakes.erase(cupcakes.begin() + c);
+
+				//decrease player health
+				player.decreaseHealth();
+				//print-out health for testing purposes
+				std::cout << "Health: " << player.getHealth() << "\n";
+			}
+
+			//if the cupcake is clicked on
+			if (cupcakes[c].getGlobalBounds().contains(m_mousePosition))
+			{
+				//increase player points
+				player.increasePoints();
+				//print-out points for testing purposes
+				std::cout << "Points: " << player.getPoints() << "\n";
+
+				//delete cupcake sprite
+				cupcakes.erase(cupcakes.begin() + c);
+			}
+		}
+	}
+}
+
+
+void Engine::updateMousePosition()
+{
+	m_mousePosition_Window = sf::Mouse::getPosition(m_Window);
+	m_mousePosition = m_Window.mapPixelToCoords(m_mousePosition_Window);
 
 }
--- ConsoleApplication1/Mouse.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Engine.h"
-
-void Engine::updateMousePosition()
-{
-	m_mousePosition_Window = sf::Mouse::getPosition(m_Window);
-	m_mousePosition = m_Window.mapPixelToCoords(m_mousePosition_Window);
-
-}
