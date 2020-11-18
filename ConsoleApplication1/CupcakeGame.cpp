33b35732cf241e77f273e375ccfcd483713524ea
Lime_Coconut <cosmina.iosifescu@gmail.com>
2020-11-18 10:28:28 +0100

    removed unnecessary includes from CupcakeGame.cpp

--- ConsoleApplication1/CupcakeGame.cpp
+++ ConsoleApplication1/CupcakeGame.cpp
@@ -1,8 +1,6 @@
 /*A simple game where the player has to click on
 sprites of cupcakes that randomly fall on the screen to gain points.
 */
-#include<SFML/Graphics.hpp>
-#include<iostream>
 #include"Engine.h"
 
 int main()
